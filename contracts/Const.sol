e70b050ba3b73938bd2290d5534dc6b95e22c479
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-21 19:06:41 +0530

    Update Const.sol

--- contracts/Const.sol
+++ contracts/Const.sol
@@ -2,11 +2,5 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Const {
-    uint256 public a;
-    uint256 public b;
-
-    constructor(uint256 _a, uint256 _b) {
-        a = _a;
-        b = _b;
-    }
+    
 }
